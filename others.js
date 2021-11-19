3c7c46bda5798c6aa238d1a433c34ca159ecd5f7
Johnson Ojo <johnsonojo89@gmail.com>
2021-11-19 08:32:02 +0100

    add to others file

--- others.js
+++ others.js
@@ -28,3 +28,20 @@ function divisibleSumPair(n, k, ar) {
 
 console.log(divisibleSumPair(5, [1, 2, 3, 4, 5, 6]));
 console.log(divisibleSumPair(3, [1, 3, 2, 6, 1, 2]));
+
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])); // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
