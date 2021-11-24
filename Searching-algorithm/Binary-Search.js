270048550037fda37edab9f1549f172fa5ac278f
Johnson Ojo <johnsonojo89@gmail.com>
2021-11-24 12:16:15 +0100

    add binary search file

---
+++ Searching-algorithm/Binary-Search.js
@@ -0,0 +1,21 @@
+/**
+ * Binary search only works on sorted arrays.It is a much faster form of search.Rather than
+ * eliminating one element at a time, you can eliminate half of the remaining elements at a time.
+ *
+ */
+
+function binarySearch(arr, value) {
+  // arr = arr.sort();
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+  while (arr[middle] !== value && left <= right) {
+    if (value > arr[middle]) left = middle + 1;
+    else right = middle - 1;
+    middle = Math.floor((left + right) / 2);
+  }
+  return arr[middle] === value ? middle : -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
