64014f2cad2b5274039faf45eb68990a12f88097
johnsonojo <johnsonojo89@gmail.com>
2022-08-13 14:50:52 +0000

    add push method

---
+++ Data-Structures/Doubly-Linked-List/doublyLinkedList.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // PUSH(add a new node to the end of the list)
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push(10);
+list.push(11);
+console.log(list);
