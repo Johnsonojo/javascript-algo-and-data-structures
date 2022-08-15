dedce962b86fa530d26f2da1d89e16f4615b2ac2
johnsonojo <johnsonojo89@gmail.com>
2022-08-15 08:49:50 +0000

    cleanup pop,shift and unShift methods

--- Data-Structures/Doubly-Linked-List/doublyLinkedList.js
+++ Data-Structures/Doubly-Linked-List/doublyLinkedList.js
@@ -30,37 +30,47 @@ class DoublyLinkedList {
 
   // POP(remove the last node from the list)
   pop() {
+    let tailToPop = this.tail;
     if (!this.head) return undefined;
-    if (this.head === this.tail) {
+    if (this.length === 1) {
       this.head = null;
       this.tail = null;
-      this.length = 0;
     } else {
-      let current = this.head;
-      let newTail = current;
-      while (current.next) {
-        newTail = current;
-        current = current.next;
-      }
-      this.tail = newTail.prev;
-      newTail.prev = null;
-      newTail.next = null;
-      this.length--;
-      return current;
+      this.tail = tailToPop.prev;
+      this.tail.next = null;
+      tailToPop.prev = null;
     }
+    this.length--;
+    return tailToPop;
   }
 
-  // SHIFT(add a node to the beginning of the linked list)
-  shift(val) {
+  // SHIFT(remove a node from the beginning of the linked list)
+  shift() {
+    if (!this.head) return undefined;
+    let headToRemove = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = headToRemove.next;
+      this.head.prev = null;
+      headToRemove.next = null;
+    }
+    this.length--;
+    return headToRemove;
+  }
+
+  // UNSHIFT(add a node to the beginning of the linked list)
+  unShift(val) {
     let newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
       let current = this.head;
-      this.head = newNode;
-      newNode.next = current;
       current.prev = newNode;
+      newNode.next = current;
+      this.head = newNode;
     }
     this.length++;
     return this;
@@ -70,6 +80,7 @@ class DoublyLinkedList {
 const list = new DoublyLinkedList();
 list.push(10);
 list.push(11);
-console.log(list.pop());
-list.shift(30);
+list.pop();
+list.shift();
+list.unShift(30);
 console.log(list);
