0474ccdcf9a26d9f0df2af0b88d3f86414e8ac20
johnsonojo <johnsonojo89@gmail.com>
2022-09-09 17:00:59 +0100

    add recursive power solution

---
+++ Recursion/power.js
@@ -0,0 +1,16 @@
+power;
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.This fu
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  if (exponent === 1) {
+    return base;
+  }
+  return base * power(base, exponent - 1);
+}
