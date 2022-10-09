e4476faaabbd4dc517182bf98788ace727704433
Johnson Ojo <johnsonojo89@gmail.com>
2022-10-09 13:53:28 +0100

    add find method and leetcode 701 solution

--- Data-Structures/Binary-Search-Tree/BinarySearchTree.js
+++ Data-Structures/Binary-Search-Tree/BinarySearchTree.js
@@ -12,6 +12,17 @@
  * and the right sub-tree and can be defined as −
  * left_subtree (keys)  ≤  node (key)  ≤  right_subtree (keys)
  *
+ *
+ * The biggest advantage of binary search trees over other data structures is that the related
+ * operations such as search, minimum and maximum can be very efficient.
+ * If the tree is balanced, each of these operations can be done in O(log n) time,
+ * where n is the number of nodes in the tree.
+ *
+ * Big O of Binary Search Tree
+ * Access - O(log n)
+ * Search - O(log n)
+ * Insert - O(log n)
+ *
  */
 
 class Node {
@@ -51,6 +62,24 @@ class BST {
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return null;
+    let current = this.root;
+    let found = false;
+    // if (current.value === value) return current;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return null;
+    return current;
+  }
 }
 
 let newBST = new BST();
@@ -59,5 +88,32 @@ newBST.insert(10);
 newBST.insert(5);
 newBST.insert(2);
 newBST.insert(11);
-
+console.log(newBST.find(11));
 console.log(newBST);
+
+// Leetcode 701 solution
+var insertIntoBST = function (root, val) {
+  let newNode = new TreeNode(val);
+  if (root === null) {
+    root = newNode;
+    return root;
+  }
+  let current = root;
+  while (true) {
+    if (val < current.val) {
+      if (current.left === null) {
+        current.left = newNode;
+        return root;
+      } else {
+        current = current.left;
+      }
+    } else {
+      if (current.right === null) {
+        current.right = newNode;
+        return root;
+      } else {
+        current = current.right;
+      }
+    }
+  }
+};
