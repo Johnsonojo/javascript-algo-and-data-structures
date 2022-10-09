56c0d0b67fc09c3a998463b8a992657d93482de3
Johnson Ojo <johnsonojo89@gmail.com>
2022-10-09 12:07:01 +0100

    add trees overview

---
+++ Data-Structures/Binary-Search-Tree/Tree.js
@@ -0,0 +1,32 @@
+/**
+ * A tree is a data structure that contains a root and child nodes.
+ * The root is the top node in a tree.
+ * Child nodes are nodes directly connected to another node when moving away from the Root.
+ * A child node cannot point to another child node.
+ * Every node can only have one parent.
+ *
+ *
+ * Terminology:
+ * Root - The top node in a tree.
+ * Child - A node directly connected to another node when moving away from the Root.
+ * Parent - The converse notion of a child.
+ * Siblings - A group of nodes with the same parent.
+ * Leaf - A node with no children.
+ * Edge - The connection between one node and another.
+ *
+ * Use cases:
+ * HTML DOM
+ * Network Routing
+ * Abstract Syntax Tree
+ * Artificial Intelligence
+ * Folders in Operating Systems
+ * Computer File Systems
+ * JSON
+ * And more...
+ *
+ * The Big O of trees
+ * Insertion - O(log n)
+ * Searching - O(log n)
+ * Access - O(n)
+ *
+ */
