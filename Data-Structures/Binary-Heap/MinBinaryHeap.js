1a644ebc6dbb22dfba00a70e215604a15407a320
Johnson Ojo <johnsonojo89@gmail.com>
2023-03-06 16:21:17 +0100

    added extractMin method for min binary heap class

--- Data-Structures/Binary-Heap/MinBinaryHeap.js
+++ Data-Structures/Binary-Heap/MinBinaryHeap.js
@@ -20,9 +20,53 @@ class MinBinaryHeap {
     this.values.push(value);
     this.bubbleUp();
   }
+
+  sinkDown() {
+    let idx = 0;
+    let length = this.values.length;
+    let element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild < element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild < element) ||
+          (swap !== null && rightChild < leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+
+  extractMin() {
+    let min = this.values[0];
+    let end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
 }
 
 let newMinBinaryHeap = new MinBinaryHeap();
+
 newMinBinaryHeap.insert(41);
 newMinBinaryHeap.insert(39);
 newMinBinaryHeap.insert(33);
@@ -30,5 +74,12 @@ newMinBinaryHeap.insert(18);
 newMinBinaryHeap.insert(27);
 newMinBinaryHeap.insert(12);
 newMinBinaryHeap.insert(55);
-
+console.log(newMinBinaryHeap.values);
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
+console.log(newMinBinaryHeap.extractMin());
 console.log(newMinBinaryHeap.values);
